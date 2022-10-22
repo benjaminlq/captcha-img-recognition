29eda7b8dde4cd725a0d2e236f03d69c5fde2967
benjaminlq <99384454+benjaminlq@users.noreply.github.com>
2022-10-22 14:08:53 +0800

    Training Script

--- src/dev/train.py
+++ src/dev/train.py
@@ -1,21 +1,32 @@
 import argparse
 from dev.models import CTC
 from dev.dataset import CaptchaDataloader
+import os
 import torch
 import torch.nn as nn
 import config
 from tqdm import tqdm
-from config import DEVICE
+from config import DEVICE, MODEL_PATH
 import matplotlib.pyplot as plt
 from dev import utils
+from pprint import pprint
 
-def train(model, no_epochs, train_loader, val_loader, learning_rate, decode_dict):
+def train(model, no_epochs, train_loader, val_loader, learning_rate, decode_dict, load_model = False):
     optimizer = torch.optim.Adam(params = model.parameters(), lr = learning_rate)
-    model.to(DEVICE)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = 0.8, patience = 5, verbose = True)
     
     train_loss_history = []
     val_loss_history = []
-    
+
+    if not MODEL_PATH.exists():
+        MODEL_PATH.mkdir()
+
+    if load_model:
+        utils.load_model(model, str(MODEL_PATH/"model.pt"))
+
+    print(f"Model Training using {DEVICE}")
+    model.to(DEVICE)
+
     for epoch in range(no_epochs):
         epoch_loss = 0
         model.train()
@@ -51,6 +62,10 @@ def train(model, no_epochs, train_loader, val_loader, learning_rate, decode_dict
         print(f"Epoch {epoch+1}: Train Loss = {epoch_loss/len(train_loader)}, Val Loss = {val_loss}, Val Accuracy = {val_accuracy}")
         train_loss_history.append(epoch_loss/len(train_loader))
         val_loss_history.append(val_loss)
+
+        scheduler.step(val_loss)
+
+        utils.save_model(model, str(MODEL_PATH/"model.pt"))
     
     plt.figure()
     plt.plot(train_loss_history, color = "red", label = "Training Loss")
@@ -59,7 +74,7 @@ def train(model, no_epochs, train_loader, val_loader, learning_rate, decode_dict
     plt.ylabel("Loss")
     plt.title("Train and Val Loss vs Epoch")
     plt.legend()
-    plt.savefig("History.png")    
+    plt.savefig(str(MODEL_PATH/"History.png"))    
     
 def eval(model, val_loader, decode_dict):
     model.eval()
@@ -67,6 +82,7 @@ def eval(model, val_loader, decode_dict):
     correct_count = 0
     total_count = 0
     tk_eval = tqdm(val_loader, total = len(val_loader))
+
     with torch.no_grad():
         for images, targets, labels in tk_eval:
             log_probs = model(images.to(DEVICE))
@@ -88,11 +104,14 @@ def eval(model, val_loader, decode_dict):
                                        target_lengths.to(DEVICE)
                                        )
             val_loss += loss.item()
-            _, preds = utils.greedy_decode(log_probs, decode_dict)
+            _, preds_full, preds = utils.greedy_decode(log_probs.permute(1,0,2), decode_dict)
             for i in range(len(labels)):
                 total_count += 1
                 if labels[i] == preds[i]:
                     correct_count += 1
+
+        combined = list(zip(preds_full, preds, labels))
+        pprint(combined[:10])
             
     val_accuracy = correct_count/total_count * 100
     
@@ -104,9 +123,11 @@ if __name__ == "__main__":
     parser.add_argument("-bs", "--batch_size", type=int, default=config.BATCH_SIZE)
     parser.add_argument("-lr", "--learning_rate", type=float, default=config.LEARNING_RATE)
     parser.add_argument("-ep", "--epochs", type=int, default=config.EPOCHS)
-    
+    parser.add_argument("-l", "--load", type=bool, default=False)
+
     args = parser.parse_args()
-    
+
+    utils.seed_everything()
     data_loader = CaptchaDataloader(batch_size=args.batch_size,
                                     resize = (config.IMG_HEIGHT, config.IMG_WIDTH))
     train_loader = data_loader.train_loader()
@@ -114,5 +135,5 @@ if __name__ == "__main__":
     model = CTC(data_loader.full_dataset.vocab_size)
     
     train(model, args.epochs, train_loader, val_loader, args.learning_rate,
-          data_loader.full_dataset.id2char)
+          data_loader.full_dataset.id2char, load_model = args.load)
     
--- src/dev/utils.py
+++ src/dev/utils.py
@@ -1,7 +1,16 @@
 import torch
 from typing import Dict, Sequence
-import torch.nn.functional as F
+import os
+import torch
 import numpy as np
+import random
+
+def save_model(model, path):
+    torch.save(model.state_dict(), path)
+    print("Model Successfully saved at", path)
+
+def load_model(model, path):
+    model.load_state_dict(torch.load(path))
 
 def collapse(sequence: Sequence, blank_symbol: int = 0):
     curr_idx = None
@@ -19,15 +28,27 @@ def greedy_decode(log_probs: torch.Tensor, decode_dict: Dict[int, str]):
     preds_id_full = preds_id_full.detach().cpu().numpy()
     
     preds_ids = []
+    preds_full = []
     preds = []
     
     for pred_id in preds_id_full:
         pred_collapsed = collapse(pred_id)
         preds_ids.append(pred_collapsed)
+        pred_sequence_full = "".join([decode_dict[idx] for idx in pred_id])
+        preds_full.append(pred_sequence_full)
         pred_sequence = "".join([decode_dict[idx] for idx in pred_collapsed])
         preds.append(pred_sequence)
-    return preds_ids, preds
+    return preds_ids, preds_full, preds
 
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    
 def beam_search():
     return 
 
